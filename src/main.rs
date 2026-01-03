1ceb58b1d52bffa09113f86d2caa22844bf33a6a
yuuki.ueyama <ueyuki.hatamochi@gmail.com>
2026-01-03 23:25:39 +0900

    条件分岐を追加

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,20 @@ fn main () {
     let hitpoint : f32 = 75.5;
     let flg : bool = true;
 
-    println!("名前：{} レベル：{} 体力：{} 生存フラグ：{}",name, level, hitpoint, flg)
+    println!("名前：{} レベル：{} 体力：{} 生存フラグ：{}",name, level, hitpoint, flg);
+
+    if level >= 30 {
+        println!("あなたはベテランプレイヤー");
+    } else {
+        println!("まだ始まったばかり");
+    }
+
+    let mut count = 0;
+    loop {
+        count += 1;
+        if count == 3{
+            println!("ループ終了");
+            break;
+        }
+    }
 }
