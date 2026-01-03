4bb92acbfc7e13ea9fe65e370d281638f3432a1c
yuuki.ueyama <ueyuki.hatamochi@gmail.com>
2026-01-03 23:18:04 +0900

    first commit: データ型練習

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main () {
+    let name = "hatamochi";
+    let level: u32 = 33;
+    let hitpoint : f32 = 75.5;
+    let flg : bool = true;
+
+    println!("名前：{} レベル：{} 体力：{} 生存フラグ：{}",name, level, hitpoint, flg)
+}
