6e4bf42dadc07d01300eeb766595658d7fde44fa
yuuki.ueyama <ueyuki.hatamochi@gmail.com>
2026-01-06 22:28:42 +0900

    所有権1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-    // 1. 名前を定義（後で変更はしません）
-    let name = "hatamochi";
-
-    // 2. 体力を定義（途中で減らしたいです）
-    let mut hp: i32 = 100;
-
-    println!("プレイヤー：{}、残りHP：{}", name, hp);
-
-    // ダメージを受ける関数を呼び出す
-    let new_hp = take_damage(hp);
-
-    println!("ダメージを受けた！ 残りHP：{}", new_hp);
+    let a = String::from("Rust");
+    print_message(&a);//貸与
+    println!("もう一回：{}", a);
 }
 
-// ダメージを計算する関数（引数から10引いた値を返したい）
-fn take_damage(current_hp: i32) -> i32 {
-    current_hp - 10
+fn print_message(meg: &String){
+    println!("メッセージを受け取りました：{}", meg);
 }
